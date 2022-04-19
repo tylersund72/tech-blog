5e58f7680e4b2ea1ba25bdab2941f277f9283649
Tyler <tylersundquist3@gmail.com>
2022-04-18 20:24:18 -0600

    more code refactor, need to work on handlebar views next

--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -1,18 +1,27 @@
+const { DELETE } = require("sequelize/types/lib/query-types");
+
 async function deleteFormHandler(event) {
   event.preventDefault();
 
-  const id = window.location.toString().split('/')[
-    window.location.toString().split('/').length - 1
+  const id = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
   ];
-  const response = await fetch(`/api/posts/${id}`, {
-    method: 'DELETE'
-  });
 
-  if (response.ok) {
-    document.location.replace('/dashboard/');
-  } else {
-    alert(response.statusText);
+  const verifyDelete = confirm("Delete?");
+
+  if (verifyDelete) {
+    const response = await fetch(`/api/posts${id}`, {
+      method: "DELETE",
+    });
+
+    if (response.ok) {
+      document.location.replace("/dashboard/");
+    } else {
+      alert(response.statusText);
+    }
   }
 }
 
-document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
+document
+  .querySelector(".delete-post-btn")
+  .addEventListener("click", deleteFormHandler);
--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -2,24 +2,30 @@ async function editFormHandler(event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="post-title"]').value.trim();
-  const id = window.location.toString().split('/')[
-    window.location.toString().split('/').length - 1
+  const post_content = document
+    .querySelector('textarea[name="post-content"]')
+    .value.trim();
+  const id = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
   ];
   const response = await fetch(`/api/posts/${id}`, {
-    method: 'PUT',
+    method: "PUT",
     body: JSON.stringify({
-      title
+      title,
+      post_content,
     }),
     headers: {
-      'Content-Type': 'application/json'
-    }
+      "Content-Type": "application/json",
+    },
   });
 
   if (response.ok) {
-    document.location.replace('/dashboard/');
+    document.location.replace("/dashboard/");
   } else {
     alert(response.statusText);
   }
 }
 
-document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
+document
+  .querySelector(".edit-post-form")
+  .addEventListener("submit", editFormHandler);
